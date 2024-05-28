05420b41321347c8936f865cfe0e2f66aa17f214
LAPikachu <131661768+LAPikachu@users.noreply.github.com>
2024-05-28 10:37:11 +0200

    task 3 finished, NiAl_2D.py creates heatmaps now

--- code/NiAl_2D.py
+++ code/NiAl_2D.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 def div_grad_2D(c0, dx):
     tmp = np.copy(c0)
@@ -21,7 +22,7 @@ dx = 1.0e-8
 N = 100
 c1 = 0.165
 c2 = 0.23
-K = 1e-6
+K = 2e-6
 x = np.linspace(0, 1.0e-8*N, N, dtype=np.float64)
 y = np.linspace(0, 1.0e-8*N, N, dtype=np.float64)
 timesteps = 1000
@@ -41,18 +42,28 @@ def run_sim( f_0, K, M, dt, c1, c2):
         c_next[:,0] = c_next[:,1]
         c_next[:,N-1] = c_next[:,N-2]
         c0 = np.copy(c_next)
-        if j == 0 or j%250 ==0:
+        if j == 0 or j%(timesteps//4) == 0:
             c_save_list.append(c0)
             t_save_list.append(j*dt)
     return c_save_list, t_save_list
 
 if __name__ == '__main__':
-    fig, axs = plt.subplots(1, 4) 
-    plt.rcParams['text.usetex'] = True
-    plt.rcParams['figure.constrained_layout.use'] = True
     c_save_list, t_save_list = run_sim(f_0 = f_0, K = K, M=M, dt=dt, c1=c1, c2=c2)
-    for i, ax in enumerate(axs):
-        axs[i] = sns.heatmap(c_save_list[i],xticklabels=False, yticklabels=False, cmap="crest")
-        axs[i].set_title('t = {} s'.format(t_save_list[i]))
+
+    # Plotting
+    vmin = c1 +0.025
+    vmax = c2 -0.025
+    fig, axs = plt.subplots(ncols=5, gridspec_kw={'width_ratios': [1, 1, 1, 1, 0.05]}, figsize=(25, 5))
+    sns.heatmap(c_save_list[0], ax=axs[0], cbar=False, vmin=vmin, vmax=vmax) 
+    sns.heatmap(c_save_list[1], ax=axs[1], cbar=False, vmin=vmin, vmax=vmax) 
+    sns.heatmap(c_save_list[2], ax=axs[2], cbar=False, vmin=vmin, vmax=vmax) 
+    sns.heatmap(c_save_list[3], ax=axs[3], cbar=False, vmin=vmin, vmax=vmax) 
+    for i,ax in enumerate(axs[:-1]):
+        ax.axis('off')
+        ax.set_title('t = {} s'.format(t_save_list[i]))
+    fig.colorbar(axs[1].collections[0], cax=axs[4])
+    axs[4].set_ylabel('Concentration')
+
+    fig.savefig('report/graphics/NiAl_2D.png')
     plt.show()
     print("cutoff prevention")
